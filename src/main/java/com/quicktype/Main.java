2a27a583324984e2098f023c4227a6f53153bf11
leegao <lg342@cornell.edu>
2017-01-11 00:26:33 -0800

    Recursively find in directory

--- src/main/java/com/quicktype/Main.java
+++ src/main/java/com/quicktype/Main.java
@@ -1,15 +1,17 @@
 package com.quicktype;
 
-import com.quicktype.parsing.Parser;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class Main {
   @Option(name = "-I", metaVar = "include")
@@ -25,7 +27,15 @@ public class Main {
   private void driver(String[] args) throws CmdLineException, IOException, ExecutionException, InterruptedException {
     CmdLineParser parser = new CmdLineParser(this);
     parser.parseArgument(args);
-    IndexingContext index = Index.index(inputFiles);
-    System.err.println(index.compiledTrees[0]);
+    List<String> inputs = new ArrayList<>();
+    for (String file : inputFiles) {
+      List<String> files = Files.find(Paths.get(file), 999, (p, bfa) -> true)
+          .map(path -> path.toAbsolutePath().toFile().toString())
+          .filter(path -> path.endsWith(".java"))
+          .collect(Collectors.toList());
+      inputs.addAll(files);
+    }
+    IndexingContext index = Index.index(inputs);
+    System.err.println(index.compiledTrees[3]);
   }
 }
